14990be7e036a6937122fce4b1ec89fea9238828
michal-trb <michaltrybulec@gmail.com>
2024-10-26 11:01:33 +0200

    full screenshot contains all monitors

--- Services/CaptureServices/AreaScreenshot.cs
+++ Services/CaptureServices/AreaScreenshot.cs
@@ -1,12 +1,9 @@
 ﻿namespace screenerWpf.Services.CaptureServices;
 
-using global::Helpers.DpiHelper;
+using screenerWpf.Sevices.CaptureServices;
 using System;
 using System.Drawing;
 using System.Runtime.InteropServices;
-using System.Windows;
-using System.Windows.Forms;
-using Size = System.Drawing.Size;
 
 /// <summary>
 /// Provides methods to capture screenshots of the entire screen or specific areas.
@@ -26,44 +23,13 @@ public class AreaScreenshot
     private static extern bool ReleaseDC(IntPtr hWnd, IntPtr hDC);
 
     /// <summary>
-    /// Captures a screenshot of the entire desktop screen.
-    /// </summary>
-    /// <returns>A <see cref="Bitmap"/> representing the entire screen.</returns>
-    public static Bitmap CaptureScreen()
-    {
-        DpiHelper.UpdateDpi();
-        var currentDpi = DpiHelper.CurrentDpi;
-
-        // Calculate the width and height based on DPI
-        int screenWidth = (int)(SystemParameters.VirtualScreenWidth * currentDpi.DpiX / 96);
-        int screenHeight = (int)(SystemParameters.VirtualScreenHeight * currentDpi.DpiY / 96);
-
-        // Create a bitmap with the screen dimensions
-        Bitmap bitmap = new Bitmap(screenWidth, screenHeight);
-
-        // Capture the screen onto the bitmap
-        using (Graphics g = Graphics.FromImage(bitmap))
-        {
-            g.CopyFromScreen(
-                SystemInformation.VirtualScreen.X,
-                SystemInformation.VirtualScreen.Y,
-                0,
-                0,
-                SystemInformation.VirtualScreen.Size,
-                CopyPixelOperation.SourceCopy);
-        }
-
-        return bitmap;
-    }
-
-    /// <summary>
     /// Captures a screenshot of a specific rectangular area of the screen.
     /// </summary>
     /// <param name="area">The area of the screen to capture.</param>
     /// <returns>A <see cref="Bitmap"/> of the specified area.</returns>
     public static Bitmap CaptureArea(Rectangle area)
     {
-        return CropBitmap(CaptureScreen(), area);
+        return CropBitmap(FullScreenshot.CaptureScreen(), area);
     }
 
     /// <summary>
--- Services/CaptureServices/FullScreenshot.cs
+++ Services/CaptureServices/FullScreenshot.cs
@@ -3,6 +3,7 @@
 using global::Helpers.DpiHelper;
 using System.Drawing;
 using System.Windows;
+using System.Windows.Forms;
 using Size = System.Drawing.Size;
 
 /// <summary>
@@ -16,15 +17,12 @@ public class FullScreenshot
     /// <returns>A <see cref="Bitmap"/> containing the screenshot of the entire primary screen.</returns>
     public static Bitmap CaptureScreen()
     {
-        // Get the DPI scaling factor
-        var presentationSource = PresentationSource.FromVisual(Application.Current.MainWindow);
-
         DpiHelper.UpdateDpi();
         var currentDpi = DpiHelper.CurrentDpi;
 
         // Calculate the width and height based on DPI
-        int screenWidth = (int)(SystemParameters.PrimaryScreenWidth * currentDpi.DpiX / 96);
-        int screenHeight = (int)(SystemParameters.PrimaryScreenHeight * currentDpi.DpiY / 96);
+        int screenWidth = (int)(SystemParameters.VirtualScreenWidth * currentDpi.DpiX / 96);
+        int screenHeight = (int)(SystemParameters.VirtualScreenHeight * currentDpi.DpiY / 96);
 
         // Create a bitmap with the screen dimensions
         Bitmap bitmap = new Bitmap(screenWidth, screenHeight);
@@ -32,7 +30,13 @@ public class FullScreenshot
         // Capture the screen onto the bitmap
         using (Graphics g = Graphics.FromImage(bitmap))
         {
-            g.CopyFromScreen(0, 0, 0, 0, new Size(screenWidth, screenHeight));
+            g.CopyFromScreen(
+                SystemInformation.VirtualScreen.X,
+                SystemInformation.VirtualScreen.Y,
+                0,
+                0,
+                SystemInformation.VirtualScreen.Size,
+                CopyPixelOperation.SourceCopy);
         }
 
         return bitmap;
